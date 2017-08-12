9dbb30f247bf03bdcefc911f417423aed6a8f615
Rei Lin <temisrei@gmail.com>
2017-08-12 23:18:14 +0800

    重構 NewAccountComponent 使用 AccountsService 服務元件

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,19 +1,25 @@
 // import { Component, EventEmitter, Output } from '@angular/core';
 import { Component } from '@angular/core';
 import { LoggingService } from '../logging.service';
+import { AccountsService } from '../accounts.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
+  providers: [LoggingService, AccountsService]
 })
 export class NewAccountComponent {
   // @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
 
-  constructor(private loggingService: LoggingService) {}
+  constructor(private loggingService: LoggingService,
+              private accountsServce: AccountsService) {}
 
   onCreateAccount(accountName: string, accountStatus: string) {
+    this.accountsServce.onAccountAdded({
+      name: accountName,
+      status: accountStatus
+    });
     // this.accountAdded.emit({
     //   name: accountName,
     //   status: accountStatus
